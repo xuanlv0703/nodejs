04bb70ab6357b9939e54ed35e197898695459c5f
Xuan Le <levanxuan@gmail.com>
2016-08-26 14:17:59 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -8,7 +8,7 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 app.get('/', function (req, res) {
-    res.render('index.html');
+    res.send('index.html');
 });
 
 
