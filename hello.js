e1a552c79f5cea846e506ec2e8b20941d5266e0f
Xuan Le <levanxuan@gmail.com>
2016-08-26 14:22:45 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,11 @@
 //  OpenShift sample Node application
-var express = require('express');
-var app     = express();
+
+var express = require('express'),
+    app     = express(),
+    eps     = require('ejs');
+    
+
+app.engine('html', require('ejs').renderFile);
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -8,7 +13,7 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 app.get('/', function (req, res) {
-    res.send('index.html');
+    res.render('index.html');
 });
 
 
